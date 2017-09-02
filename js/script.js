d2650c78222528bf079fa3bc5ba1aa47ed3d359f
jbadan <jenna.badanowski@gmail.com>
2017-09-01 18:05:13 -0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
-console.log('Hello, front end');
+
+  var solutionDiv = document.getElementById("solution");
+  var fahrenheitRadioButton = document.getElementById("radioFaren");
+  var celciusRadioButton = document.getElementById("radioCel");
+
+document.getElementById("submit").onclick = function(){
+  var temp = document.getElementById("numberEntered").value;
+  switch(true){
+    case fahrenheitRadioButton.checked === true:
+      solutionDiv.textContent = ((parseFloat(temp)-32)/1.8).toFixed(2)+" °C";
+      break;
+    case celciusRadioButton.checked === true:
+      solutionDiv.textContent = (parseFloat(temp)*1.8+32).toFixed(2)+" °F";
+      break;
+    }
+  switch(true){
+    case ((fahrenheitRadioButton.checked === true && temp <= 32)||(celciusRadioButton.checked === true && temp <= 0)):
+      solutionDiv.style.background = "blue";
+      var kitty =document.createElement("img");
+        kitty.src="img/coldCat.jpg";
+        kitty.style.margin = "50px";
+        solutionDiv.appendChild(kitty);
+      break;
+    case ((fahrenheitRadioButton.checked === true && temp >= 85)||(celciusRadioButton.checked === true && temp >= 30)):
+      solutionDiv.style.background = "red";
+      var hotKitty =document.createElement("img");
+        hotKitty.src="img/hotCat.jpg";
+        hotKitty.style.margin = "50px";
+        solutionDiv.appendChild(hotKitty);
+      break;
+    default:
+      var happyKitty = document.createElement("img");
+      happyKitty.src="img/happyCat.jpg";
+      happyKitty.style.margin = "50px";
+      solutionDiv.appendChild(happyKitty);
+    }
+  }
+
+document.getElementById("reset").onclick = function(){
+  solutionDiv.textContent = "";
+  solutionDiv.style.background = "white";
+}
